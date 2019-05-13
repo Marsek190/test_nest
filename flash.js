a47482ab7dfe0da0bf028b42c5a0727ac1291a24
jonik-do-it <d.arkhipov1997@yandex.ru>
2019-05-14 02:44:12 +0300

    using anonymous classes - bad idea)

--- flash.js
+++ flash.js
@@ -10,33 +10,34 @@
  */
 export const flash = (options = { rendered: false }) => (req, res, next) => {
     if (req.session === undefined) throw new Error('req.flash requires sessions');	
-    if (req.flash) return next();   
-    req.flash = new class {
-        constructor () {
-            this._oldInput = req.body || req.query || {};
-            this._storage = req.session.flash = req.session.flash || {};
-        }
+    if (req.flash) return next();
+    // way for basic functionality   
+    // req.flash = function () {
+    // };
+    req.flash = {
+        _oldInput: req.body || req.query || {},
+        _storage: req.session.flash = req.session.flash || {},
 
         get oldInput() {
             const oldInput = this._oldInput;
             this._oldInput = {};
             return oldInput;
-        }
+        },
         
         get all() {
             req.session.flash = {};
             return this._storage;
-        }
+        },
 
         get(name) {
             const item = Reflect.get(this._storage, name);
             Reflect.set(this._storage, name, undefined);
             return item || [];
-        }
+        },
 
-        set(name, value) {           
+        set(name, value) {
             (this._storage[name] = this._storage[name] || []).push(...(Array.isArray(value) ? value : [value]));
-        }
+        },
 
         empty() {
             return Object.is(this._storage, {});
